d69a44f6ec52cb649961225a45a62f72832ba457
DSPianoid <astrinleonid@digitalstringspiano.com>
2025-09-08 19:52:20 +0300

    Update venv-activate.bat

--- venv-activate.bat
+++ venv-activate.bat
@@ -1,7 +1,8 @@
 @echo off
 echo Completing the requirements installation...
 cd PianoidCore
-call venv\Scripts\activate.bat
+python3 -m venv .venv
+call .venv\Scripts\activate.bat
 venv\Scripts\python.exe -m pip install -r requirements.txt
 echo.
 echo Installation completed!
